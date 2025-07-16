17da5e7f117a1620f6fa810d17ccea3e9615483a
mirano0915 <miranoitf21@gmail.com>
2025-07-16 13:08:07 +0900

    springbootの環境用意しました

---
+++ kintai_app/src/main/java/com/kintai/enshu/KintaiAppApplication.java
@@ -0,0 +1,13 @@
+package com.kintai.enshu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KintaiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KintaiAppApplication.class, args);
+	}
+
+}
