b6fc8ef14f10b15d8798d19aa0a274ecd3d9b701
mirano0915 <miranoitf21@gmail.com>
2025-07-17 13:55:13 +0900

    方とDBを直しました

--- kintai_app/src/main/java/com/kintai/Service/PayrollService.java
+++ kintai_app/src/main/java/com/kintai/Service/PayrollService.java
@@ -3,11 +3,13 @@ package com.kintai.Service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
 import com.kintai.Dao.HourlyWagesDAO;
 import com.kintai.Entity.HourlyWagesEntity;
 
+@Service
 public class PayrollService {
 
 	
