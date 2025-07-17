0b2d7d67f05401b40bab0061bc70dfe1838bb934
KeisukeKuno <guiyoujiuye8@gmail.com>
2025-07-17 10:14:54 +0900

    IndexServiceにAttendancesDaoをAutowired

--- kintai_app/src/main/java/com/kintai/Service/IndexService.java
+++ kintai_app/src/main/java/com/kintai/Service/IndexService.java
@@ -1,5 +1,16 @@
 package com.kintai.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kintai.Dao.AttendancesDAO;
+
+@Service
 public class IndexService {
 
+	//DB操作が必要なためAttendancesDaoを準備
+	@Autowired
+	private AttendancesDAO attendancesDAO;
+	
+	
 }
