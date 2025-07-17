2aa529bf6ef63645f8bef8efa68eac3b4911cf67
mirano0915 <miranoitf21@gmail.com>
2025-07-17 14:45:02 +0900

    stampsDAOのひな型作成

--- kintai_app/src/main/java/com/kintai/Dao/StampsDAO.java
+++ kintai_app/src/main/java/com/kintai/Dao/StampsDAO.java
@@ -1,8 +1,35 @@
 package com.kintai.Dao;
 
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.kintai.Entity.AttendancesEntity;
 
 @Repository
 public class StampsDAO {
 
+	//DB操作専用オブジェクトを準備
+	private final JdbcTemplate db;
+
+	public StampsDAO(JdbcTemplate db) {
+		this.db = db;
+	}
+
+	//打刻申請を追加
+	public void updateAttendanceTime() {
+
+	}
+
+	//打刻申請を削除
+	public void deleteAttendanceTime() {
+
+	}
+
+	//勤怠テーブルと打刻変更テーブルを外部結合
+
+	public List<AttendancesEntity> showAttendanceAgreeTable(){
+		
+	}
 }
