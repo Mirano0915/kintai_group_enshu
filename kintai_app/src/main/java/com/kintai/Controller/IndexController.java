b52d30565882131da479ebd5b98d1d6f79d3ed1e
KeisukeKuno <guiyoujiuye8@gmail.com>
2025-07-17 09:43:33 +0900

    indexServiceをAutowired

--- kintai_app/src/main/java/com/kintai/Controller/IndexController.java
+++ kintai_app/src/main/java/com/kintai/Controller/IndexController.java
@@ -1,19 +1,23 @@
 package com.kintai.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kintai.Service.IndexService;
 
 @Controller
 public class IndexController {
 
 	//Viewのサービスオブジェクト準備
-//	@Autowired
-//	private IndexService indexService;
+	@Autowired
+	private IndexService indexService;
 	
 	
 	//勤怠管理画面表示
 	@RequestMapping("/")
 	public String read() {
+		
 	return "index";
 	}
 }
