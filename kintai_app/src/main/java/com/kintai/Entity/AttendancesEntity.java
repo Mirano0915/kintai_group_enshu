5af2f7edd46d1f7eae39a0728ea2bfc0d6b8bf77
mirano0915 <miranoitf21@gmail.com>
2025-07-17 10:15:10 +0900

    entityをつくりました

--- kintai_app/src/main/java/com/kintai/Entity/AttendancesEntity.java
+++ kintai_app/src/main/java/com/kintai/Entity/AttendancesEntity.java
@@ -1,5 +1,20 @@
 package com.kintai.Entity;
 
-public class AttendancesEntity {
+import java.time.LocalDate;
+import java.time.LocalTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class AttendancesEntity{
+	private Long nameId;  //Primary Key
+	private String name;
+	private LocalTime checkinTime;
+	private LocalTime checkoutTime;
+	private LocalDate date;
 }
--- kintai_app/src/main/java/com/kintai/Entity/StampsEntity.java
+++ kintai_app/src/main/java/com/kintai/Entity/StampsEntity.java
@@ -1,5 +1,25 @@
 package com.kintai.Entity;
 
-public class StampsEntity {
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class StampsEntity {
+	private Long stampId; //Primary Key
+	private Long nameId;  //Foreign Key
+	private String name;
+	private LocalTime checkinTime;
+	private LocalTime checkoutTime;
+	private LocalDate date;
+	private LocalTime preCheckinTime;
+	private LocalTime preCkoutTime;
+	private String comment;
 }
