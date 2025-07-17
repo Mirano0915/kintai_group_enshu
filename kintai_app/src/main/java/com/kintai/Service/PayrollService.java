c477b9d8a701e1971e034515a2b8629578716ef2
KeisukeKuno <guiyoujiuye8@gmail.com>
2025-07-17 10:38:56 +0900

    payrollServiceを簡単に準備

--- kintai_app/src/main/java/com/kintai/Service/PayrollService.java
+++ kintai_app/src/main/java/com/kintai/Service/PayrollService.java
@@ -1,5 +1,20 @@
 package com.kintai.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+
+import com.kintai.Dao.HourlyWagesDAO;
+
 public class PayrollService {
 
+	
+	//DB操作が必要なためHourlyWagesDAOを準備
+	@Autowired
+	private HourlyWagesDAO hourlywagesDAO;
+	
+	
+	//勤怠テーブルと時給テーブルを呼びだし＆給与計算
+	public void payroll(Model model) {
+		
+	}
 }
