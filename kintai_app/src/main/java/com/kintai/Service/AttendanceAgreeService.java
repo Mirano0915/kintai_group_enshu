fe7cc6846e28c7b9b2a77b40714cd7be20eb1c08
mirano0915 <miranoitf21@gmail.com>
2025-07-18 10:41:15 +0900

    AttendanceAgreeServiceができた

--- kintai_app/src/main/java/com/kintai/Service/AttendanceAgreeService.java
+++ kintai_app/src/main/java/com/kintai/Service/AttendanceAgreeService.java
@@ -1,5 +1,23 @@
 package com.kintai.Service;
 
-public class AttendanceAgreeService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+
+import com.kintai.Dao.StampsDAO;
+import com.kintai.Entity.StampsEntity;
+
+public class AttendanceAgreeService {
+	//DB操作が必要なためStampsDAOを準備
+		@Autowired
+		private StampsDAO stampsDAO;
+		
+		
+		//勤怠テーブルと時給テーブルを呼びだし＆給与計算
+		public void payroll(Model model) {
+			List<StampsEntity>resultDb2 = stampsDAO.showAttendanceAgreeTable();
+			System.out.println(resultDb2.get(0).getNameId());
+		
+}
 }
