b5ffe044bcc9b6ed22ea0731e490d6b4cbb6e46f
mirano0915 <miranoitf21@gmail.com>
2025-07-18 10:35:42 +0900

    stampsDAOに変更申請一覧表示を追加

--- kintai_app/src/main/java/com/kintai/Dao/StampsDAO.java
+++ kintai_app/src/main/java/com/kintai/Dao/StampsDAO.java
@@ -1,11 +1,15 @@
 package com.kintai.Dao;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import com.kintai.Entity.AttendancesEntity;
+import com.kintai.Entity.StampsEntity;
 import com.kintai.Form.AttendanceChangeForm;
 
 @Repository
@@ -33,6 +37,7 @@ public class StampsDAO {
 	}
 
 	//打刻申請を削除
+	//これはajax対応あるのでいらないかも
 	public void deleteAttendanceTime(Long stampId) {
 		
 		String sql = "DELETE FROM stamps WHERE stamp_id=?";
@@ -41,11 +46,42 @@ public class StampsDAO {
 
 	}
 
-	//勤怠テーブルと打刻変更テーブルを外部結合
+	//勤怠テーブルと打刻変更テーブルを内部結合
 
-	public List<AttendancesEntity> showAttendanceAgreeTable() {
+	public List<StampsEntity> showAttendanceAgreeTable() {
 		
+		String sql ="SELECT"
+				+ "NAME, "
+				+ "CHECKIN_TIME, "
+				+ "CHECKOUT_TIME, "
+				+ "PRE_CHECKIN_TIME, "
+				+ "PRE_CHECKOUT_TIME, "
+				+ "DATE "
+				+ "COMMENT "
+				+ "FROM STAMPS "
+				+ "INNER JOIN "
+				+ "ATTENDANCES ON STAMPS.NAME_ID = ATTENDANCES.NAME_ID";
 		
+		List<Map<String, Object>> resultDb1 = db.queryForList(sql);
+		
+		List<StampsEntity>resultDb2 = new ArrayList<StampsEntity>();//勤怠履歴を入れておくリスト
 
+
+		for(Map<String, Object> result1:resultDb1) {
+			StampsEntity entitydb = new StampsEntity();
+			
+			entitydb.setName((String)result1.get("name"));
+			entitydb.setCheckinTime((Time)result1.get("checkin_time"));
+			entitydb.setCheckoutTime((Time)result1.get("checkout_time"));
+			entitydb.setPreCheckinTime((Time)result1.get("pre_checkin_time"));
+			entitydb.setPreCheckoutTime((Time)result1.get("pre_checkout_time"));
+			entitydb.setDate((Date)result1.get("date"));
+			entitydb.setComment((String)result1.get("comment"));
+
+			
+			resultDb2.add(entitydb);
+			
+		}
+		return resultDb2;
 	}
 }
