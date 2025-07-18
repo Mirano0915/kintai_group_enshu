6e246be15f8e6bdd98c1b5b29b4a9dc76c88ea66
mirano0915 <miranoitf21@gmail.com>
2025-07-18 11:16:02 +0900

    AttendanceAgree.htmlで一覧がみられるようになりました

--- kintai_app/src/main/java/com/kintai/Controller/AttendanceAgreeController.java
+++ kintai_app/src/main/java/com/kintai/Controller/AttendanceAgreeController.java
@@ -1,5 +1,26 @@
 package com.kintai.Controller;
 
-public class AttendanceAgreeController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.kintai.Entity.StampsEntity;
+import com.kintai.Service.AttendanceAgreeService;
+
+@Controller
+public class AttendanceAgreeController {
+	//AttendanceAgreeのサービスオブジェクトを準備
+	@Autowired
+	private AttendanceAgreeService attendanceAgreeService;
+
+	//勤怠変更申請ページ遷移＆一覧
+	@GetMapping("/attendanceAgree")
+	public String attendanceAgree(Model model) {
+		List<StampsEntity> changeRequests = attendanceAgreeService.getAttendanceAgreeList();
+        model.addAttribute("changeRequests", changeRequests);
+		return "attendanceAgree";
+	}
 }
--- kintai_app/src/main/java/com/kintai/Service/AttendanceAgreeService.java
+++ kintai_app/src/main/java/com/kintai/Service/AttendanceAgreeService.java
@@ -3,21 +3,23 @@ package com.kintai.Service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
+import org.springframework.stereotype.Service;
 
 import com.kintai.Dao.StampsDAO;
 import com.kintai.Entity.StampsEntity;
 
+@Service
 public class AttendanceAgreeService {
 	//DB操作が必要なためStampsDAOを準備
-		@Autowired
-		private StampsDAO stampsDAO;
+	@Autowired
+	private StampsDAO stampsDAO;
+
+	//勤怠テーブルと時給テーブルを呼びだし＆給与計算
+	public List<StampsEntity> getAttendanceAgreeList() {
+		List<StampsEntity> resultDb2 = stampsDAO.showAttendanceAgreeTable();
+		System.out.println(resultDb2.get(0).getNameId());
 		
-		
-		//勤怠テーブルと時給テーブルを呼びだし＆給与計算
-		public void payroll(Model model) {
-			List<StampsEntity>resultDb2 = stampsDAO.showAttendanceAgreeTable();
-			System.out.println(resultDb2.get(0).getNameId());
-		
-}
+		return resultDb2;
+
+	}
 }
