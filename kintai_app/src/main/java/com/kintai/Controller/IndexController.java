68f8dfcee39da2e83e9cff05c8930c0b8e95e214
mirano0915 <miranoitf21@gmail.com>
2025-07-25 10:07:58 +0900

    ページ遷移の方法をすこしかえました

--- kintai_app/src/main/java/com/kintai/Controller/IndexController.java
+++ kintai_app/src/main/java/com/kintai/Controller/IndexController.java
@@ -1,12 +1,12 @@
 package com.kintai.Controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kintai.Dao.AttendancesDAO;
 import com.kintai.Dao.HourlyWagesDAO;
@@ -18,66 +18,76 @@ public class IndexController {
 	//Viewのサービスオブジェクト準備
 	@Autowired
 	private IndexService indexService;
-	
-	
+
 	@Autowired
 	private HourlyWagesDAO hourlyWagesDAO;
-	
+
 	@Autowired
 	private AttendancesDAO attendancesDAO;
-	
-	
+
 	//勤怠管理画面表示
 	@GetMapping("/")
 	public String attendanceAgree(Model model) {
-	
+
 		indexService.readName(model);
 		System.out.println("メソッド呼ばれた");
 		return "index";
 	}
-	
-	
-	//出勤処理（出勤ボタン）
+
 	@RequestMapping("/checkin")
-	public String checkin(@RequestParam("employeeName") String name, Model model) {
+	public String checkin(
+			@RequestParam("employeeName") String name,
+			RedirectAttributes redirectAttributes,
+			Model model) {
 
-	    Long nameId = hourlyWagesDAO.getNameIdByName(name);
+		Long nameId = hourlyWagesDAO.getNameIdByName(name);
 
-	    if (attendancesDAO.hasCheckedInToday(nameId)) {
-	        model.addAttribute("errorMessage", "今日はもう出勤済みです！");
-	        indexService.readName(model);
-	        return "index";
+		if (attendancesDAO.hasCheckedInToday(nameId)) {
+			model.addAttribute("errorMessage", "今日はもう出勤済みです！");
+			indexService.readName(model);
+			return "index";
 
-	    } else if (attendancesDAO.forgotCheckedInToday(nameId)) {
-	        model.addAttribute("errorMessage", "不正な操作です　勤務時間の変更は申請を行って下さい");
-	        indexService.readName(model);
-	        return "index";
+		} else if (attendancesDAO.forgotCheckedInToday(nameId)) {
+			model.addAttribute("errorMessage", "不正な操作です　勤務時間の変更は申請を行って下さい");
+			indexService.readName(model);
+			return "index";
+		}
 
-	    }
+		// 出勤処理
+		String checkTime = indexService.checkin(nameId);
 
-	    model.addAttribute("checkTime",indexService.checkin(nameId)); // nameIdで処理するにゃ！
-	    
-	    return "complete";
+		// 完了画面に渡す情報を一時保存（リダイレクト用）
+		redirectAttributes.addAttribute("type", "checkin");
+		redirectAttributes.addAttribute("employeeName", name);
+		redirectAttributes.addAttribute("checkTime", checkTime);
+
+		return "redirect:/complete";
 	}
 
-	
 	//退勤処理(退勤ボタン)
 	@RequestMapping("/checkout")
-	public String checkout(@RequestParam("employeeName") String name, Model model) {
-		
-		 Long nameId = hourlyWagesDAO.getNameIdByName(name);
+	public String checkout(
+			@RequestParam("employeeName") String name,
+			RedirectAttributes redirectAttributes,
+			Model model) {
 
-		    if (attendancesDAO.hasCheckedoutToday(nameId)) {
-		        model.addAttribute("errorMessage", "今日はもう退勤済みです！");
-		        indexService.readName(model);
-		        return "index";
+		Long nameId = hourlyWagesDAO.getNameIdByName(name);
 
-		    }
-		model.addAttribute("checkTime",indexService.checkout(nameId));
-		return "complete";
+		if (attendancesDAO.hasCheckedoutToday(nameId)) {
+			model.addAttribute("errorMessage", "今日はもう退勤済みです！");
+			indexService.readName(model);
+			return "index";
+
+		}
+		// 出勤処理
+		String checkTime = indexService.checkout(nameId);
+
+		// 完了画面に渡す情報を一時保存（リダイレクト用）
+		redirectAttributes.addAttribute("type", "checkin");
+		redirectAttributes.addAttribute("employeeName", name);
+		redirectAttributes.addAttribute("checkTime", checkTime);
+
+		return "redirect:/complete";
 	}
-	
-	
-	
-	
+
 }
